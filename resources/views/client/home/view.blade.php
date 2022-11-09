1ea5132168a495d709700c1bb888af840a51b10e
zelvad <vladislav.zelenyk@scalemeup.com>
2022-11-09 21:17:28 +0200

    fix

--- resources/views/client/home/view.blade.php
+++ resources/views/client/home/view.blade.php
@@ -42,51 +42,7 @@
                     <div>
                         <div class="form__title" style="margin-bottom: 50px">Введите свои данные и нажмите <span>продолжить!</span></div>
                         <form class="form" action="https://safevpn.online/register-subscribe">
-                            <div class="form__group">
-                                <div class="input__wrapper  input__wrapper_firstname">
-                                    <input type="text" name="firstname" id='firstname' placeholder=" " class="form__input" required autocomplete="off">
-                                    <span class="custom-placeholder">Имя</span>
-                                    <svg width="15" height="16" viewBox="0 0 15 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M9.13608 10.7098C9.08265 10.1291 9.10374 9.72406 9.10374 9.19391C9.36671 9.0561 9.8378 8.17719 9.91655 7.43469C10.1233 7.41781 10.4495 7.21672 10.5451 6.42078C10.5958 5.99328 10.3919 5.75281 10.2667 5.67688C10.6028 4.66578 11.3017 1.53688 8.97577 1.21344C8.7353 0.792971 8.12218 0.580627 7.32624 0.580627C4.13827 0.63969 3.75436 2.98813 4.45327 5.67688C4.32811 5.75281 4.12421 5.99328 4.17483 6.42078C4.27186 7.21672 4.59671 7.41781 4.80343 7.43469C4.88218 8.17719 5.37155 9.0561 5.63593 9.19391C5.63593 9.72406 5.65561 10.1291 5.60218 10.7098C4.97218 12.4058 0.716864 11.9291 0.519989 15.2H14.2C14.0031 11.9291 9.76608 12.4058 9.13608 10.7098Z" fill="white"/>
-                                    </svg>
-                                    <span class="error__text"></span>
-                                </div>
-                                <div class="input__wrapper input__wrapper_lastname">
-                                    <input type="text" name="lastname" id='lastname' placeholder=" " class="form__input" required autocomplete="off">
-                                    <span class="custom-placeholder">Фамилия</span>
-                                    <svg width="15" height="16" viewBox="0 0 15 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M9.13608 10.7098C9.08265 10.1291 9.10374 9.72406 9.10374 9.19391C9.36671 9.0561 9.8378 8.17719 9.91655 7.43469C10.1233 7.41781 10.4495 7.21672 10.5451 6.42078C10.5958 5.99328 10.3919 5.75281 10.2667 5.67688C10.6028 4.66578 11.3017 1.53688 8.97577 1.21344C8.7353 0.792971 8.12218 0.580627 7.32624 0.580627C4.13827 0.63969 3.75436 2.98813 4.45327 5.67688C4.32811 5.75281 4.12421 5.99328 4.17483 6.42078C4.27186 7.21672 4.59671 7.41781 4.80343 7.43469C4.88218 8.17719 5.37155 9.0561 5.63593 9.19391C5.63593 9.72406 5.65561 10.1291 5.60218 10.7098C4.97218 12.4058 0.716864 11.9291 0.519989 15.2H14.2C14.0031 11.9291 9.76608 12.4058 9.13608 10.7098Z" fill="white"/>
-                                    </svg>
-                                    <span class="error__text"></span>
-                                </div>
-                            </div>
-                            <div class="form__group" style='margin-top: 22px'>
-                                <div class="input__wrapper input__wrapper_phone">
-                                    <input id="phone" type="phone" name="phone" placeholder=" " class="form__input" required autocomplete="off">
-                                    <span class="custom-placeholder">Телефон</span>
-                                    <svg width="11" height="18" viewBox="0 0 11 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path fill-rule="evenodd" clip-rule="evenodd" d="M0 1.76471C0 0.790086 0.735055 0 1.64179 0H9.35821C10.2649 0 11 0.790086 11 1.76471V16.2353C11 17.2099 10.2649 18 9.35821 18H1.64179C0.735055 18 0 17.2099 0 16.2353V1.76471ZM1.64179 0.882353C1.18842 0.882353 0.820896 1.2774 0.820896 1.76471V16.2353C0.820896 16.7226 1.18842 17.1176 1.64179 17.1176H9.35821C9.81158 17.1176 10.1791 16.7226 10.1791 16.2353V1.76471C10.1791 1.2774 9.81158 0.882353 9.35821 0.882353H9.0709C8.78972 1.13186 8.65124 1.64678 8.5732 2.22721C8.51161 2.68523 8.16277 3.04412 7.73223 3.04412H3.26777C2.83723 3.04412 2.48839 2.68523 2.4268 2.22721C2.34876 1.64678 2.21028 1.13186 1.9291 0.882353H1.64179ZM3.0246 1.12619C3.13192 1.43995 3.19627 1.78126 3.23926 2.101C3.24307 2.12937 3.25516 2.14863 3.26452 2.15787C3.26653 2.15986 3.26909 2.16176 3.26909 2.16176H7.73091C7.73091 2.16176 7.73347 2.15986 7.73548 2.15787C7.74484 2.14863 7.75693 2.12937 7.76074 2.101C7.80374 1.78126 7.86808 1.43995 7.97541 1.12619C8.00238 1.04733 8.03373 0.965262 8.0705 0.882353H2.9295C2.96627 0.965262 2.99762 1.04733 3.0246 1.12619Z" fill="white" />
-                                        <path d="M4.02239 1.54412C4.02239 1.37356 4.15102 1.23529 4.3097 1.23529H5.62313C5.78181 1.23529 5.91045 1.37356 5.91045 1.54412C5.91045 1.71468 5.78181 1.85294 5.62313 1.85294H4.3097C4.15102 1.85294 4.02239 1.71468 4.02239 1.54412Z" fill="white" />
-                                        <path d="M6.23881 1.54412C6.23881 1.37356 6.36744 1.23529 6.52612 1.23529C6.6848 1.23529 6.81343 1.37356 6.81343 1.54412C6.81343 1.71468 6.6848 1.85294 6.52612 1.85294C6.36744 1.85294 6.23881 1.71468 6.23881 1.54412Z" fill="white" />
-                                    </svg>
-                                    <span class="error__text">Введите свой телефон</span>
-                                </div>
-                                <div class="input__wrapper input__wrapper_email">
-                                    <input id="email" type="email" name="email" placeholder=" " class="form__input" required autocomplete="off">
-                                    <span class="custom-placeholder">E-mail</span>
-                                    <svg width="18" height="14" viewBox="0 0 18 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M0 0.52002V13.48H18V0.52002H0ZM16.56 3.85846L9 8.14471L1.44 3.85846V3.00908L9 7.29533L16.56 3.00908V3.85846Z" fill="white"/>
-                                    </svg>                    <span class="error__text">Введите свой email-адрес</span>
-                                </div>
-                            </div>
-
-                            <div class="form__footer">
-                                <div class="home__terms">
-                                    Нажимая кнопку “Продолжить” <br>и “Принять участие” вы соглашаетесь <br>с <a href="#" onclick="openModal(this)" data-target="modalTerms">Публичная оферта</a>
-                                </div>
-                                <button type="submit" class="button button-continue button-blank">Продолжить</button>
-                                <div class="button__blur"></div>
-                            </div>
+                            <button type="submit" class="button button-continue button-blank">Продолжить</button>
                         </form>
                     </div>
                     <!-- 					STEP 1 FORM END -->
